f58d5a8201626e599c3a1c5fd229ec23bfad5fde
sarojsudarshan <sarojsudarshan18@gmail.com>
2026-06-30 15:25:52 +0000

    Frontend setup and Configured Spring Boot backend with MySQL database

---
+++ backend/craftcart-api/src/main/java/com/craftcart/craftcart_api/CraftcartApiApplication.java
@@ -0,0 +1,13 @@
+package com.craftcart.craftcart_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftcartApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraftcartApiApplication.class, args);
+	}
+
+}
---
+++ backend/craftcart-api/src/test/java/com/craftcart/craftcart_api/CraftcartApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.craftcart.craftcart_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftcartApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
