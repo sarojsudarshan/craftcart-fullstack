cc6ad4416717456e59586b7676f45553e521c4e8
sarojsudarshan <sarojsudarshan18@gmail.com>
2026-08-24 08:41:34 +0000

    Create home page

--- frontend/craftcart-ui/src/app/app.ts
+++ frontend/craftcart-ui/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+import { Home } from './pages/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+ imports: [Header, Footer, Home],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
